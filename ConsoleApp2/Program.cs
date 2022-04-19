3ec40718c0b381cbc510a2b235dccf7f6976ecbc
gunayyz <100687596+gunayyz@users.noreply.github.com>
2022-04-20 00:38:29 +0400

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {   
+        public static void Main(string[] args)
+        {
+            IAccount account = new IAccount();
+            Console.WriteLine("1.Programı başlat");
+            Console.WriteLine("2.Programdan çıx");
+            byte choose = Convert.ToByte(Console.ReadLine());
+            switch (choose)
+            {
+                case 1:
+                    account.Start();
+                    break;
+               case 2:
+                    account.End();
+                    break;
+            }
+
+        }
+    }
+}
---
+++ ConsoleApp2/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class User
+    {
+        public string FullName;
+        public string Password;
+        public string Email;
+
+        //Encapsulation
+        //private string email;
+
+        //public string email
+        //{
+        //    get
+        //    {
+        //        return email;
+        //    }
+        //    set
+        //    {
+        //        email = value;
+        //    }
+        //}
+        public User(string FullName, string PassWord, string Email)
+        {
+            this.FullName = FullName;
+            this.Password = PassWord;
+            this.Email = Email;
+        }
+    }
+}
